442c79ba92781c3407986a892af1165bae11bb47
Tamim-Amin <tamimamin13579@gmail.com>
2025-10-07 16:35:54 +0600

    list and tuple

---
+++ list_and_tuple_generator.py
@@ -0,0 +1,14 @@
+# Write a Python program that accepts a sequence of comma-separated numbers from the user 
+#and generates a list and a tuple of those numbers.
+# Sample data : 3, 5, 7, 23
+# Output :
+# List : ['3', ' 5', ' 7', ' 23']
+# Tuple : ('3', ' 5', ' 7', ' 23')
+
+input=input("Enter a sequence of comma-separated numbers: ")
+
+list=input.split(",")
+tuple=tuple(list)
+
+print("List: ",list)
+print("Tuple: ",tuple)
