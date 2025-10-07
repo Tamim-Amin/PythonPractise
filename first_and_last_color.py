5d03a751bf9d4567e3283e2ed7cf6c0fc44f5ea4
Tamim-Amin <tamimamin13579@gmail.com>
2025-10-07 21:38:11 +0600

    list first and last elements

---
+++ first_and_last_color.py
@@ -0,0 +1,4 @@
+# Write a Python program to display the first and last colors from the following list.
+# color_list = ["Red","Green","White" ,"Black"]
+color_list = ["Red","Green","White" ,"Black"]
+print(f"first color: {color_list[0]}, last color: {color_list[-1]}")
