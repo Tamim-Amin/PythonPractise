0e9842c0f820ff4840fdaed9acd3e47367c944e8
Tamim-Amin <tamimamin13579@gmail.com>
2025-10-12 21:55:59 +0600

    volume of the sphere

---
+++ sphere_volume_check.py
@@ -0,0 +1,6 @@
+# Write a Python program to get the volume of a sphere
+from math import pi
+
+radious=float(input("Enter the radious of sphere: "))
+
+print(f"The volume of sphere is : {4/3*pi*radious**3}")
